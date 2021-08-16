2a732f39ec9c9df7b9f7f5a1da24dcf88f44a11d
ziye <980492289@qq.com>
2021-08-16 21:34:27 +0800

    removed social link

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
   title: 'GoldenRoom',
-  author: 'Biasia',
+  author: 'GoldenRoom',
   email: 'lzybiasia@gmail.com',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
@@ -17,7 +17,7 @@ const BLOG = {
   showArchive: false,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: '',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
